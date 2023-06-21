1ea1c4b5b58aedc2ad926c66c30037763df0471a
Alexander Perez <alexander.pereze02@gmail.com>
2023-06-20 21:16:31 -0400

    check handler

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,13 +2,19 @@ import { type Todo as TodoType, type TodoId } from '../types'
 
 interface Props extends TodoType {
   remove: ({ id }: TodoId) => void
+  toggleComplete: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
   // remove: (id: (string | number)) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, title, completed, remove }) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, remove, toggleComplete }) => {
+  // to use event handlers of inputs, we need to use React.ChangeEvent<HTMLInputElement>
+  const handleCheckBox = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    toggleComplete({ id, completed: e.target.checked }) // or just put this logic inside of onChange attribute
+  }
+
   return (
     <div className="view">
-      <input type="checkbox" className="toggle" checked={completed} onChange={() => {}} />
+      <input type="checkbox" className="toggle" checked={completed} onChange={(e) => { handleCheckBox(e) }} />
       <label>{title}</label>
       <button className='destroy' onClick={() => { remove({ id }) }}></button>
     </div>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,18 +1,19 @@
-import { type ListOfTodos, type TodoId } from '../types'
+import { type ListOfTodos, type TodoId, type Todo as TodoType } from '../types'
 import { Todo } from './Todo'
 
 interface TodosProps {
   todos: ListOfTodos
   remove: ({ id }: TodoId) => void
+  toggleComplete: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
   // remove: (id: (string | number)) => void
 }
 
-export const Todos: React.FC<TodosProps> = ({ todos, remove }) => {
+export const Todos: React.FC<TodosProps> = ({ todos, remove, toggleComplete }) => {
   return (
     <ul className='todo-list'>
       {todos.map(todo => (
         <li className={`${todo.completed ? 'completed' : ''}`} key={todo.id}>
-          <Todo key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} remove={remove} />
+          <Todo key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} remove={remove} toggleComplete={toggleComplete} />
         </li>
       ))}
     </ul>
